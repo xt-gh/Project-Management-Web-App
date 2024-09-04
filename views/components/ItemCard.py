e7aa06c1f65f35192ef5a837e898290a9bbd3b38
Kaiz <84890519+Kaiz404@users.noreply.github.com>
2024-09-04 11:15:51 +0800

    Fixed Add task feature

--- views/components/ItemCard.py
+++ views/components/ItemCard.py
@@ -1,26 +1,51 @@
 from flet import *
+from data.manage_data import Data
 
-class ItemCard(Row):
-    def __init__(self, task_name="test", task_description="", priority="", story_points="", tags="", stage="", assignee="", *args, **kwargs):
+class ItemCard(Column):
+    def __init__(self, item_id, handle_detailed_view=None):
         super().__init__()
-        self.task_name = task_name
-        self.task_description = task_description
-        self.priority = priority
-        self.story_points = story_points
-        self.tags = tags
-        self.stage = stage
-        self.assignee = assignee
+        self.item_id = item_id
+        self.data = Data()
+        item =self.data.get_product_backlog_item(self.item_id) 
+
+        self.task_name = item["task_name"]
+        self.task_description = item["description"]
+        self.priority = item["priority"]
+        self.story_points = item["story_points"]
+        self.tags = item["tags"]
+        self.stage = item["stage"]
+        self.assignee = item["assignee"]
+
+        self.handle_detailed_view = handle_detailed_view
 
 
     def build(self):
+
+        tags = Row()
+        task_title = Text(self.task_name, color="black", size=20)
+        details = Row([
+            IconButton(
+                icon=icons.MORE_HORIZ_ROUNDED,
+                icon_color="black",
+                on_click=self.handle_detailed_view
+            )
+        ])
+        
+
+
         return Container(
-            content=Row([
-                Text(self.task_name, color="black", size=24),
-                IconButton(
-                    icon=icons.MORE_HORIZ_ROUNDED,
-                    icon_color="black"
-                )
-            ], alignment=MainAxisAlignment.SPACE_BETWEEN),
+            content=Column([
+                            Row([
+                                    Text(self.task_name, color="black", size=20),
+                                    IconButton(
+                                        icon=icons.MORE_HORIZ_ROUNDED,
+                                        icon_color="black",
+                                        on_click=self.handle_detailed_view
+                                    )
+                                    ], alignment=MainAxisAlignment.SPACE_BETWEEN),
+                            
+                            ]
+            ),
             bgcolor="#BABDE2",
             border=border.all(2, "#374375"),
             border_radius=border_radius.all(10),
